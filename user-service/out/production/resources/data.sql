e5aee96d24841dec4d2811d8c0b7bb747efd8097
Rayest <634178463@qq.com>
2018-10-18 15:14:33 +0800

    Delete data.sql

--- user-service/out/production/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into sc_user (id, username, name, age, balance) values (1, 'account1', '张三', 20, 100.00);
-insert into sc_user (id, username, name, age, balance) values (2, 'account2', '李四', 30, 200.00);
-insert into sc_user (id, username, name, age, balance) values (3, 'account3', '王五', 40, 300.00);
