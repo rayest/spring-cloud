7c270e5c38aba246a195a573162b19239040c8e6
lirui <634178463@qq.com>
2018-10-20 23:28:04 +0800

    分离

---
+++ movie-service/src/main/java/mobi/rayson/feign/FeignClientFallbackFactory.java
@@ -0,0 +1,11 @@
+package mobi.rayson.feign;
+
+/***
+ *  Created with IntelliJ IDEA.
+ *  User:  lirui
+ *  Date:  2018-10-20
+ *  Time: 下午11:27
+ *  Description:
+ **/
+public class FeignClientFallbackFactory {
+}
