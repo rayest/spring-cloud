df9fc46ecf3d894897b7b965836977442371f408
lirui <634178463@qq.com>
2018-10-21 11:10:26 +0800

    添加zuul过滤器

--- zuul-server/src/main/java/mobi/rayson/filter/PreRequestLogFilter.java
+++ zuul-server/src/main/java/mobi/rayson/filter/PreRequestLogFilter.java
@@ -1,5 +1,12 @@
 package mobi.rayson.filter;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
 /***
  *  Created with IntelliJ IDEA.
  *  User:  lirui
@@ -7,5 +14,30 @@ package mobi.rayson.filter;
  *  Time: 上午11:00
  *  Description:
  **/
-public class PreRequestLogFilter extends  {
+public class PreRequestLogFilter extends ZuulFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(PreRequestLogFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        logger.info("send {} to {}", request.getMethod(), request.getRequestURL().toString());
+        return null;
+    }
 }
