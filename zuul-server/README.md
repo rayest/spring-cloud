5d58a08535b44668efc501d6390dcdc58b505976
lirui <634178463@qq.com>
2018-12-26 15:27:33 +0800

    添加zuul路由和请求日志输出.

--- zuul-server/README.md
+++ zuul-server/README.md
@@ -2,6 +2,7 @@
 
 ### 访问
 > http://ZUUL-SERVICE-HOST:ZUUL-SERVICE-PORT/serviceId/path, 如：http://localhost:8100/user-service/user/1
+> 也可以通过网关路由进行访问：如：http://localhost:8100/api-user/user/2 ("对外时推荐使用")
 
 > 通过 zuul 网关访问 user-service 服务
 
--- zuul-server/src/main/java/mobi/rayson/filter/PreRequestLogFilter.java
+++ zuul-server/src/main/java/mobi/rayson/filter/PreRequestLogFilter.java
@@ -2,10 +2,12 @@ package mobi.rayson.filter;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 
 /***
  *  Created with IntelliJ IDEA.
@@ -14,30 +16,81 @@ import javax.servlet.http.HttpServletRequest;
  *  Time: 上午11:00
  *  Description:
  **/
+@Slf4j
 public class PreRequestLogFilter extends ZuulFilter {
 
-    private final Logger logger = LoggerFactory.getLogger(PreRequestLogFilter.class);
+  @Override
+  public String filterType() {
+    return "pre";
+  }
 
-    @Override
-    public String filterType() {
-        return "pre";
+  @Override
+  public int filterOrder() {
+    return 1;
+  }
+
+  @Override
+  public boolean shouldFilter() {
+    return true;
+  }
+
+  //@Override
+  //public Object run() {
+  //    RequestContext requestContext = RequestContext.getCurrentContext();
+  //    HttpServletRequest request = requestContext.getRequest();
+  //    log.info("send {} to {}", request.getMethod(), request.getRequestURL().toString());
+  //    return null;
+  //}
+
+  @Override
+  public Object run() {
+    HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+    log.info("Request: >" + req.getScheme() + " " + req.getRemoteAddr() + ":" + req.getRemotePort());
+    StringBuilder params = new StringBuilder("?");
+    Enumeration<String> names = req.getParameterNames();
+    if (req.getMethod().equals("GET")) {
+      while (names.hasMoreElements()) {
+        String name = names.nextElement();
+        params.append(name);
+        params.append("=");
+        params.append(req.getParameter(name));
+        params.append("&");
+      }
     }
 
-    @Override
-    public int filterOrder() {
-        return 1;
+    if (params.length() > 0) {
+      params.delete(params.length() - 1, params.length());
+    }
+    log.info("Request: > "
+        + req.getMethod()
+        + " "
+        + req.getRequestURI()
+        + params
+        + " "
+        + req.getProtocol());
+
+    Enumeration<String> headers = req.getHeaderNames();
+
+    while (headers.hasMoreElements()) {
+      String name = headers.nextElement();
+      String value = req.getHeader(name);
+      log.info("Header: > " + name + ":" + value);
     }
 
-    @Override
-    public boolean shouldFilter() {
-        return true;
+    final RequestContext ctx = RequestContext.getCurrentContext();
+    if (!ctx.isChunkedRequestBody()) {
+      ServletInputStream inp;
+      try {
+        inp = ctx.getRequest().getInputStream();
+        String body;
+        if (inp != null) {
+          body = IOUtils.toString(inp);
+          log.info("Body: > " + body);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
-
-    @Override
-    public Object run() {
-        RequestContext requestContext = RequestContext.getCurrentContext();
-        HttpServletRequest request = requestContext.getRequest();
-        logger.info("send {} to {}", request.getMethod(), request.getRequestURL().toString());
-        return null;
-    }
+    return null;
+  }
 }
